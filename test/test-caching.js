03b11b98c4a2802bc5dd8c6bf3a56380c025fc60
A.D. Slaton <slaton.ad@gmail.com>
2016-03-24 18:27:20 +0000

    clean up formatting of example/index.js

--- test/test-caching.js
+++ test/test-caching.js
@@ -1,336 +1,338 @@
 var Caching = require('../'),
-	memoryCache = new Caching('memory'),
-	assert = require('assert');
+    memoryCache = new Caching('memory'),
+    assert = require('assert'),
+    utils = require('utils');
 
-exports['MemoryStore'] = function(beforeExit) {
-	var wroteCache = false,
-		lastResults,
-		callbacksCalled = 0,
-		key = 'hello memory '+Math.random(),
-		ttl = 500; // 1s
+exports['MemoryStore'] = function (beforeExit) {
+    var wroteCache = false,
+        lastResults,
+        callbacksCalled = 0,
+        key = utils.format('hello memory %s', Math.random()),
+        ttl = 500; // 1s
 
-	function store(next) {
-		callbacksCalled++;
-		wroteCache = true;
-		setTimeout(function() {
-			next(null, Date.now());
-		}, 200);
-	}
-	
-	// Feel the cache
-	memoryCache(key, ttl, store, function(err, results) {
-		callbacksCalled++;
-		assert.ifError(err);
-		assert.equal(typeof results, 'number');
-		assert.ok(wroteCache);
-		lastResults = results;
-		wroteCache = false;
-	});
+    function store(next) {
+        callbacksCalled++;
+        wroteCache = true;
+        setTimeout(function () {
+            next(null, Date.now());
+        }, 200);
+    }
 
-	// Try again
-	memoryCache(key, ttl, store, function(err, results) {
-		callbacksCalled++;
-		assert.ifError(err);
-		assert.equal(typeof results, 'number');
-		assert.equal(results, lastResults);
-		assert.ok(!wroteCache);
-		lastResults = results;
-		wroteCache = false;
-	});
+    // Feel the cache
+    memoryCache(key, ttl, store, function (err, results) {
+        callbacksCalled++;
+        assert.ifError(err);
+        assert.equal(typeof results, 'number');
+        assert.ok(wroteCache);
+        lastResults = results;
+        wroteCache = false;
+    });
 
-	beforeExit(function() {
-		assert.equal(callbacksCalled, 3);
-	});
+    // Try again
+    memoryCache(key, ttl, store, function (err, results) {
+        callbacksCalled++;
+        assert.ifError(err);
+        assert.equal(typeof results, 'number');
+        assert.equal(results, lastResults);
+        assert.ok(!wroteCache);
+        lastResults = results;
+        wroteCache = false;
+    });
+
+    beforeExit(function () {
+        assert.equal(callbacksCalled, 3);
+    });
 };
 
-exports['MemoryStore expiration'] = function(beforeExit) {
-	var wroteCache = false
-		, lastResults
-		, callbacksCalled = 0
-		, key = 'hello memory '+Math.random()
-		, ttl = 500; // .5s
+exports['MemoryStore expiration'] = function (beforeExit) {
+    var wroteCache = false,
+        lastResults,
+        callbacksCalled = 0,
+        key = utils.format('hello memory %s', Math.random()),
+        ttl = 500; // .5s
 
-	function store(next) {
-		callbacksCalled++;
-		wroteCache = true;
-		setTimeout(function() {
-			next(null, Date.now());
-		}, 200);
-	}
+    function store(next) {
+        callbacksCalled++;
+        wroteCache = true;
+        setTimeout(function () {
+            next(null, Date.now());
+        }, 200);
+    }
 
-	// Feel the cache
-	memoryCache(key, ttl, store, function(err, results) {
-		callbacksCalled++;
-		assert.ifError(err);
-		assert.equal(typeof results, 'number');
-		assert.ok(wroteCache);
-		lastResults = results;
-		wroteCache = false;
-	});
+    // Feel the cache
+    memoryCache(key, ttl, store, function (err, results) {
+        callbacksCalled++;
+        assert.ifError(err);
+        assert.equal(typeof results, 'number');
+        assert.ok(wroteCache);
+        lastResults = results;
+        wroteCache = false;
+    });
 
-	// Wait until the cache has expired
-	setTimeout(function() {
-		memoryCache(key, ttl, store, function(err, results) {
-			callbacksCalled++;
-			assert.ifError(err);
-			assert.equal(typeof results, 'number');
-			assert.notEqual(results, lastResults);
-			assert.ok(wroteCache);
-			lastResults = results;
-			wroteCache = false;
-		});
-	}, ttl*2);
+    // Wait until the cache has expired
+    setTimeout(function () {
+        memoryCache(key, ttl, store, function (err, results) {
+            callbacksCalled++;
+            assert.ifError(err);
+            assert.equal(typeof results, 'number');
+            assert.notEqual(results, lastResults);
+            assert.ok(wroteCache);
+            lastResults = results;
+            wroteCache = false;
+        });
+    }, ttl * 2);
 
-	beforeExit(function() {
-		assert.equal(callbacksCalled, 4);
-	});
-};
-
-
-exports['MemoryStore removal'] = function(beforeExit) {
-	var wroteCache = false
-		, callbacksCalled = 0
-		, key = 'hello rem memory '+Math.random()
-		, ttl = 500; // .5s
-
-	function store(next) {
-		callbacksCalled++;
-		wroteCache = true;
-		setTimeout(function() {
-			next(null, Date.now());
-			wroteCache = false;
-		}, 200);
-	}
-
-	// Feel the cache
-	memoryCache(key, ttl, store, function setBeforeRemoval(err, results) {
-		callbacksCalled++;
-		assert.ifError(err);
-		assert.equal(typeof results, 'number');
-		assert.ok(wroteCache);
-
-		// Remove it manually
-		memoryCache.remove(key);
-
-		// Try again
-		memoryCache(key, ttl, store, function getAfterRemoval(err, results) {
-			callbacksCalled++;
-			assert.ifError(err);
-			assert.equal(typeof results, 'number');
-			assert.ok(wroteCache);
-		});
-	});
-
-	beforeExit(function() {
-		assert.equal(callbacksCalled, 4);
-	});
+    beforeExit(function () {
+        assert.equal(callbacksCalled, 4);
+    });
 };
 
 
-exports['MemoryStore removal pattern'] = function(beforeExit) {
-	var wroteCache = false
-		, callbacksCalled = 0
-		, key = 'hello rem memory '+Math.random()
-		, ttl = 500; // .5s
+exports['MemoryStore removal'] = function (beforeExit) {
+    var wroteCache = false,
+        callbacksCalled = 0,
+        key = utils.format('hello rem memory %s', Math.random()),
+        ttl = 500; // .5s
 
-	function store(next) {
-		callbacksCalled++;
-		wroteCache = true;
-		setTimeout(function() {
-			next(null, Date.now());
-			wroteCache = false;
-		}, 200);
-	}
+    function store(next) {
+        callbacksCalled++;
+        wroteCache = true;
+        setTimeout(function () {
+            next(null, Date.now());
+            wroteCache = false;
+        }, 200);
+    }
 
-	// Feel the cache
-	memoryCache(key, ttl, store, function setBeforeRemoval(err, results) {
-		callbacksCalled++;
-		assert.ifError(err);
-		assert.equal(typeof results, 'number');
-		assert.ok(wroteCache);
+    // Feel the cache
+    memoryCache(key, ttl, store, function setBeforeRemoval(err, results) {
+        callbacksCalled++;
+        assert.ifError(err);
+        assert.equal(typeof results, 'number');
+        assert.ok(wroteCache);
 
-		// Remove it manually (using a pattern)
-		memoryCache.remove('hello rem*');
+        // Remove it manually
+        memoryCache.remove(key);
 
-		// Try again
-		memoryCache(key, ttl, store, function getAfterRemoval(err, results) {
-			callbacksCalled++;
-			assert.ifError(err);
-			assert.equal(typeof results, 'number');
-			assert.ok(wroteCache);
-		});
-	});
+        // Try again
+        memoryCache(key, ttl, store, function getAfterRemoval(err, results) {
+            callbacksCalled++;
+            assert.ifError(err);
+            assert.equal(typeof results, 'number');
+            assert.ok(wroteCache);
+        });
+    });
 
-	beforeExit(function() {
-		assert.equal(callbacksCalled, 4);
-	});
+    beforeExit(function () {
+        assert.equal(callbacksCalled, 4);
+    });
 };
 
 
-exports['RedisStore'] = function(beforeExit) {
-	var redisCache = new Caching('redis')
-		, wroteCache = false
-		, callbacksCalled = 0
-		, key = 'hello redis '+Math.random()
-		, ttl = 500; // 1s
+exports['MemoryStore removal pattern'] = function (beforeExit) {
+    var wroteCache = false,
+        callbacksCalled = 0,
+        key = utils.format('hello rem memory %s', Math.random()),
+        ttl = 500; // .5s
 
-	function store(next) {
-		callbacksCalled++;
-		wroteCache = true;
-		setTimeout(function() {
-			next(null, Date.now());
-		}, 200);
-	}
-	
-	// Feel the cache
-	redisCache(key, ttl, store, function(err, results) {
-		callbacksCalled++;
-		assert.ifError(err);
-		assert.equal(typeof results, 'number');
-		assert.ok(wroteCache);
-		wroteCache = false;
-	});
+    function store(next) {
+        callbacksCalled++;
+        wroteCache = true;
+        setTimeout(function () {
+            next(null, Date.now());
+            wroteCache = false;
+        }, 200);
+    }
 
-	// Try again
-	redisCache(key, ttl, store, function(err, results) {
-		callbacksCalled++;
-		assert.ifError(err);
-		assert.equal(typeof results, 'number');
-		assert.ok(!wroteCache);
-		redisCache.store.client.end();
-	});
+    // Feel the cache
+    memoryCache(key, ttl, store, function setBeforeRemoval(err, results) {
+        callbacksCalled++;
+        assert.ifError(err);
+        assert.equal(typeof results, 'number');
+        assert.ok(wroteCache);
 
-	beforeExit(function() {
-		assert.equal(callbacksCalled, 3);
-	});
+        // Remove it manually (using a pattern)
+        memoryCache.remove('hello rem*');
+
+        // Try again
+        memoryCache(key, ttl, store, function getAfterRemoval(err, results) {
+            callbacksCalled++;
+            assert.ifError(err);
+            assert.equal(typeof results, 'number');
+            assert.ok(wroteCache);
+        });
+    });
+
+    beforeExit(function () {
+        assert.equal(callbacksCalled, 4);
+    });
 };
 
 
-exports['RedisStore expiration'] = function(beforeExit) {
-	var redisCache = new Caching('redis')
-		, wroteCache = false
-		, callbacksCalled = 0
-		, key = 'hello redis '+Math.random()
-		, ttl = 1000; // 1s (the least possible on Redis since it only takes integer seconds)
+exports['RedisStore'] = function (beforeExit) {
+    var redisCache = new Caching('redis'),
+        wroteCache = false,
+        callbacksCalled = 0,
+        key = utils.format('hello redis %s', Math.random()),
+        ttl = 500; // 1s
 
-	function store(next) {
-		callbacksCalled++;
-		wroteCache = true;
-		setTimeout(function() {
-			next(null, Date.now());
-		}, 200);
-	}
+    function store(next) {
+        callbacksCalled++;
+        wroteCache = true;
+        setTimeout(function () {
+            next(null, Date.now());
+        }, 200);
+    }
 
-	// Feel the cache
-	redisCache(key, ttl, store, function(err, results) {
-		callbacksCalled++;
-		assert.ifError(err);
-		assert.equal(typeof results, 'number');
-		assert.ok(wroteCache);
-		wroteCache = false;
-	});
+    // Feel the cache
+    redisCache(key, ttl, store, function (err, results) {
+        callbacksCalled++;
+        assert.ifError(err);
+        assert.equal(typeof results, 'number');
+        assert.ok(wroteCache);
+        wroteCache = false;
+    });
 
-	// Wait until the cache has expired
-	var t = Date.now();
-	setTimeout(function() {
-		redisCache(key, ttl, store, function(err, results) {
-			callbacksCalled++;
-			assert.ifError(err);
-			assert.equal(typeof results, 'number');
-			assert.ok(wroteCache);
-			redisCache.store.client.end();
-		});
-	}, ttl*2);
+    // Try again
+    redisCache(key, ttl, store, function (err, results) {
+        callbacksCalled++;
+        assert.ifError(err);
+        assert.equal(typeof results, 'number');
+        assert.ok(!wroteCache);
+        redisCache.store.client.end();
+    });
 
-	beforeExit(function() {
-		assert.equal(callbacksCalled, 4);
-	});
+    beforeExit(function () {
+        assert.equal(callbacksCalled, 3);
+    });
 };
 
 
-exports['RedisStore removal'] = function(beforeExit) {
-	var redisCache = new Caching('redis')
-		, wroteCache = false
-		, callbacksCalled = 0
-		, key = 'hello rem redis '+Math.random()
-		, ttl = 500; // .5s
+exports['RedisStore expiration'] = function (beforeExit) {
+    var redisCache = new Caching('redis'),
+        wroteCache = false,
+        callbacksCalled = 0,
+        key = utils.format('hello redis %s', Math.random()),
+        ttl = 1000, // 1s (the least possible on Redis since it only takes integer seconds)
+        t;
 
-	function store(next) {
-		callbacksCalled++;
-		wroteCache = true;
-		setTimeout(function() {
-			next(null, Date.now());
-		}, 200);
-	}
+    function store(next) {
+        callbacksCalled++;
+        wroteCache = true;
+        setTimeout(function () {
+            next(null, Date.now());
+        }, 200);
+    }
 
-	// Feel the cache
-	redisCache(key, ttl, store, function setBeforeRemoval(err, results) {
-		callbacksCalled++;
-		assert.ifError(err);
-		assert.equal(typeof results, 'number');
-		assert.ok(wroteCache);
-		wroteCache = false;
+    // Feel the cache
+    redisCache(key, ttl, store, function (err, results) {
+        callbacksCalled++;
+        assert.ifError(err);
+        assert.equal(typeof results, 'number');
+        assert.ok(wroteCache);
+        wroteCache = false;
+    });
 
-		// Remove it manually
-		redisCache.remove(key);
+    // Wait until the cache has expired
+    t = Date.now();
+    setTimeout(function () {
+        redisCache(key, ttl, store, function (err, results) {
+            callbacksCalled++;
+            assert.ifError(err);
+            assert.equal(typeof results, 'number');
+            assert.ok(wroteCache);
+            redisCache.store.client.end();
+        });
+    }, ttl * 2);
 
-		// Try again (wait a little bit because the patters requires two redis commands)
-		setTimeout(function() { 
-			redisCache(key, ttl, store, function getAfterRemoval(err, results) {
-				callbacksCalled++;
-				assert.ifError(err);
-				assert.equal(typeof results, 'number');
-				assert.ok(wroteCache);
-				redisCache.store.client.end();
-			});
-		}, 50);
-	});
-
-	beforeExit(function() {
-		assert.equal(callbacksCalled, 4);
-	});
+    beforeExit(function () {
+        assert.equal(callbacksCalled, 4);
+    });
 };
 
 
-exports['RedisStore removal pattern'] = function(beforeExit) {
-	var redisCache = new Caching('redis')
-		, wroteCache = false
-		, callbacksCalled = 0
-		, key = 'hello rem redis '+Math.random()
-		, ttl = 500; // .5s
+exports['RedisStore removal'] = function (beforeExit) {
+    var redisCache = new Caching('redis'),
+        wroteCache = false,
+        callbacksCalled = 0,
+        key = utils.format('hello rem redis %s', Math.random()),
+        ttl = 500; // .5s
 
-	function store(next) {
-		callbacksCalled++;
-		wroteCache = true;
-		setTimeout(function() {
-			next(null, Date.now());
-		}, 200);
-	}
+    function store(next) {
+        callbacksCalled++;
+        wroteCache = true;
+        setTimeout(function () {
+            next(null, Date.now());
+        }, 200);
+    }
 
-	// Feel the cache
-	redisCache(key, ttl, store, function setBeforeRemoval(err, results) {
-		callbacksCalled++;
-		assert.ifError(err);
-		assert.equal(typeof results, 'number');
-		assert.ok(wroteCache);
-		wroteCache = false;
+    // Feel the cache
+    redisCache(key, ttl, store, function setBeforeRemoval(err, results) {
+        callbacksCalled++;
+        assert.ifError(err);
+        assert.equal(typeof results, 'number');
+        assert.ok(wroteCache);
+        wroteCache = false;
 
-		// Remove it manually (using a pattern)
-		redisCache.remove('hello rem*');
+        // Remove it manually
+        redisCache.remove(key);
 
-		// Try again (wait a little bit because the patters requires two redis commands)
-		setTimeout(function() { 
-			redisCache(key, ttl, store, function getAfterRemoval(err, results) {
-				callbacksCalled++
-				assert.ifError(err);
-				assert.equal(typeof results, 'number')
-				assert.ok(wroteCache)
-				redisCache.store.client.end()
-			});
-		}, 50)
-	});
+        // Try again (wait a little bit because the patters requires two redis commands)
+        setTimeout(function () {
+            redisCache(key, ttl, store, function getAfterRemoval(err, results) {
+                callbacksCalled++;
+                assert.ifError(err);
+                assert.equal(typeof results, 'number');
+                assert.ok(wroteCache);
+                redisCache.store.client.end();
+            });
+        }, 50);
+    });
 
-	beforeExit(function() {
-		assert.equal(callbacksCalled, 4);
-	});
+    beforeExit(function () {
+        assert.equal(callbacksCalled, 4);
+    });
+};
+
+
+exports['RedisStore removal pattern'] = function (beforeExit) {
+    var redisCache = new Caching('redis'),
+        wroteCache = false,
+        callbacksCalled = 0,
+        key = utils.format('hello rem redis %s', Math.random()),
+        ttl = 500; // .5s
+
+    function store(next) {
+        callbacksCalled++;
+        wroteCache = true;
+        setTimeout(function () {
+            next(null, Date.now());
+        }, 200);
+    }
+
+    // Feel the cache
+    redisCache(key, ttl, store, function setBeforeRemoval(err, results) {
+        callbacksCalled++;
+        assert.ifError(err);
+        assert.equal(typeof results, 'number');
+        assert.ok(wroteCache);
+        wroteCache = false;
+
+        // Remove it manually (using a pattern)
+        redisCache.remove('hello rem*');
+
+        // Try again (wait a little bit because the patters requires two redis commands)
+        setTimeout(function () {
+            redisCache(key, ttl, store, function getAfterRemoval(err, results) {
+                callbacksCalled++;
+                assert.ifError(err);
+                assert.equal(typeof results, 'number');
+                assert.ok(wroteCache);
+                redisCache.store.client.end();
+            });
+        }, 50);
+    });
+
+    beforeExit(function () {
+        assert.equal(callbacksCalled, 4);
+    });
 };
